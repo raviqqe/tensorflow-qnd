8db34671110ab0161704a2d1f52b050502dea5d0
Yota Toyama <raviqqe@gmail.com>
2016-12-20 11:47:57 +0900

    Add test of config module

---
+++ qnd/config_test.py
@@ -0,0 +1,26 @@
+import sys
+import unittest
+
+import tensorflow as tf
+
+from .config import *
+
+
+
+class TestConfig(unittest.TestCase):
+  def test_def_config(self):
+    config = def_config()
+    self.assertTrue(isinstance(config(), tf.contrib.learn.ClusterConfig))
+
+
+def append_argv():
+  sys.argv += [
+      "--ps_hosts", "localhost:4242",
+      "--worker_hosts", "localhost:5353",
+      "--task_type", "ps",
+      "--task_index", "0"]
+
+
+if __name__ == "__main__":
+  append_argv()
+  unittest.main(argv=["test"])
