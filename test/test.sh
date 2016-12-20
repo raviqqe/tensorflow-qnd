84c522a9b360bf30abd284106d449831ec4ed018
Yota Toyama <raviqqe@gmail.com>
2016-12-20 11:52:38 +0900

    Improve format of test log

--- test/test.sh
+++ test/test.sh
@@ -4,6 +4,7 @@ for module in $(find qnd | grep '_test\.py' \
                          | sed 's/\(qnd\/.*_test\)\.py/\1/g' \
                          | tr / .)
 do
-  echo Testing $module
+  echo "# Testing ${module%_test}"
   python3 -m $module
+  echo
 done
