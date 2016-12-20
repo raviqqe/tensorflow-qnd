5f1e48679af515193461f6045f6943d276b7d8e3
Yota Toyama <raviqqe@gmail.com>
2016-12-20 15:23:03 +0900

    Add test of inputs and fix inputs module

--- qnd/inputs.py
+++ qnd/inputs.py
@@ -1,32 +1,28 @@
-import functools
-import json
-import logging
-import os
-
 import tensorflow as tf
 import gargparse
 from gargparse import ARGS
 
 from . import util
+from .flag import add_flag, add_required_flag
 
 
 
 def add_file_flag(use):
   flag_name = "{}_file".format(use)
   add_required_flag(flag_name,
-                    help="File path of {} dataset file(s). "
-                         "Glob is accepted. (e.g. eval/*.csv)".format(use))
+                    help="File path of {} data file(s). "
+                         "Glob is accepted. (e.g. train/*.csv)".format(use))
   return flag_name
 
 
 def def_def_input_fn(use):
-  def def_input_fn(file_parser):
+  def def_input_fn():
     file_flag = add_file_flag(use)
     read_files = def_read_files()
 
     @util.func_scope
-    def input_fn():
-      return read_files(getattr(ARGS, file_flag), file_parser)
+    def input_fn(user_input_fn):
+      return read_files(getattr(ARGS, file_flag), user_input_fn)
 
     return input_fn
 
@@ -38,56 +34,24 @@ def_eval_input_fn = def_def_input_fn("eval")
 
 
 def def_read_files():
-  add_flag("batch_queue_capacity", type=int, default=64)
-  file_pattern_to_name = def_file_pattern_to_name()
+  file_pattern_to_name_queue = def_file_pattern_to_name_queue()
 
   @util.func_scope
-  def read_files(file_pattern, file_parser):
-    return monitored_queue(
-        *file_parser(file_pattern_to_name(file_pattern)),
-        queue_size_name="batches_in_queue",
-        capacity=ARGS.batch_queue_capacity)
+  def read_files(file_pattern, user_input_fn):
+    return user_input_fn(file_pattern_to_name_queue(file_pattern))
 
   return read_files
 
 
-def def_file_pattern_to_name():
+def def_file_pattern_to_name_queue():
   add_flag("num_epochs", type=int)
   add_flag("filename_queue_capacity", type=int, default=32)
 
   @util.func_scope
-  def file_pattern_to_name(pattern):
-    filename_queue = tf.train.string_input_producer(
+  def file_pattern_to_name_queue(pattern):
+    return tf.train.string_input_producer(
         tf.train.match_filenames_once(pattern),
         num_epochs=ARGS.num_epochs,
-        capacity=FLAGS.filename_queue_capacity)
+        capacity=ARGS.filename_queue_capacity)
 
-    tf.summary.scalar("filenames_in_queue", filename_queue.size())
-
-    return filename_queue.dequeue()
-
-  return file_pattern_to_names
-
-
-def def_monitored_queue():
-  @util.func_scope
-  def monitored_queue(*tensors,
-                      capacity,
-                      queue_size_name,
-                      return_queue=False):
-    queue = tf.FIFOQueue(capacity, dtypes(*tensors))
-    tf.summary.scalar(summary_name, queue.size())
-    tf.train.add_queue_runner(queue, [queue.enqueue(tensors)])
-
-    if return_queue:
-      return queue
-
-    results = queue.dequeue()
-
-    for tensor, result \
-        in zip(tensors, results if isinstance(results, list) else [results]):
-      result.set_shape(tensor.get_shape())
-
-    return results
-
-  return monitored_queue
+  return file_pattern_to_name_queue
---
+++ qnd/inputs_test.py
@@ -0,0 +1,49 @@
+import sys
+import unittest
+
+import tensorflow as tf
+
+from . import inputs
+from . import test
+
+
+
+class InputsTest(unittest.TestCase):
+  def test_def_input_fn(self):
+    for input_fn \
+        in [inputs.def_train_input_fn(), inputs.def_eval_input_fn()]:
+      # Return (tf.Tensor, tf.Tensor)
+
+      features, labels = input_fn(lambda queue: (queue.dequeue(),) * 2)
+
+      self.assertIsInstance(features, tf.Tensor)
+      self.assertIsInstance(labels, tf.Tensor)
+
+      # Return (dict, dict)
+
+      def user_input_fn(filename_queue):
+        x = filename_queue.dequeue()
+        return {"feature": x}, {"label": x}
+
+      features, labels = input_fn(user_input_fn)
+
+      self.assertIsInstance(features, dict)
+      self.assertIsInstance(labels, dict)
+
+      self._assertAreInstances([*features.keys(), *labels.keys()], str)
+      self._assertAreInstances([*features.values(), *labels.values()],
+                               tf.Tensor)
+
+  def _assertAreInstances(self, objects, klass):
+    for obj in objects:
+      self.assertIsInstance(obj, klass)
+
+
+def append_argv():
+  file_pattern = "*.md"
+  sys.argv += ["--train_file", file_pattern, "--eval_file", file_pattern]
+
+
+if __name__ == "__main__":
+  append_argv()
+  test.main()
