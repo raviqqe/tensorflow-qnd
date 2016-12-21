c024e5b08766e3e8513c259780f5aa2eca92c0e9
Yota Toyama <raviqqe@gmail.com>
2016-12-21 10:20:10 +0900

    Add tests of train and evaluate modules

---
+++ qnd/evaluate_test.py
@@ -0,0 +1,14 @@
+import unittest
+
+from . import test
+from .evaluate import *
+
+
+
+class EvaluateTest(unittest.TestCase):
+  def test_def_evaluate(self):
+    def_evaluate()
+
+
+if __name__ == "__main__":
+  test.main()
---
+++ qnd/train_test.py
@@ -0,0 +1,14 @@
+import unittest
+
+from . import test
+from .train import *
+
+
+
+class TrainTest(unittest.TestCase):
+  def test_def_train(self):
+    def_train_and_evaluate()
+
+
+if __name__ == "__main__":
+  test.main()
