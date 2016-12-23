b17fee0a2d3f08ff492239a9df3f6b2d3a9e90a0
Yota Toyama <raviqqe@gmail.com>
2016-12-23 13:25:19 +0900

    Test if Estimators accept model_fns which return ModelFnOps

--- qnd/estimator_test.py
+++ qnd/estimator_test.py
@@ -9,3 +9,8 @@ def test_def_estimator():
     test.initialize_argv(*TEST_ARGS)
     assert isinstance(def_estimator()(test.oracle_model, "output"),
                       tf.contrib.learn.Estimator)
+    assert isinstance(
+        def_estimator()(
+            tf.contrib.learn.estimators.model_fn.ModelFnOps(test.oracle_model),
+            "output"),
+        tf.contrib.learn.Estimator)
