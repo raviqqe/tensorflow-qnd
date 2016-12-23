a2d4c01557be1150acd662e1ea2f6c1b0f69524b
Yota Toyama <raviqqe@gmail.com>
2016-12-23 15:41:41 +0900

    Add documentation of flag module

--- qnd/flag.py
+++ qnd/flag.py
@@ -6,6 +6,13 @@ _FLAG_NAMES = set()
 
 
 def add_flag(name, *args, **kwargs):
+    """Add a flag.
+
+    Args:
+        name: Flag name. Real flag name will be `"--{}".format(name)`.
+        *args, **kwargs: The rest arguments are the same as
+            `argparse.add_argument()`.
+    """
     global _FLAG_NAMES
 
     if name not in _FLAG_NAMES:
@@ -14,10 +21,16 @@ def add_flag(name, *args, **kwargs):
 
 
 def add_required_flag(name, *args, **kwargs):
+    """Add a required flag.
+
+    Its interface is the same as `add_flag()` but `required=True` is set by
+    default.
+    """
     add_flag(name, *args, required=True, **kwargs)
 
 
 class FlagAdder:
+    """Manage addition of flags."""
 
     def __init__(self):
         self._flags = []
@@ -31,4 +44,9 @@ class FlagAdder:
 
     @property
     def flags(self):
+        """Get added flags.
+
+        Returns:
+            `dict` of flag names to values added by a `FlagAdder` instance.
+        """
         return {flag: getattr(FLAGS, flag) for flag in self._flags}
