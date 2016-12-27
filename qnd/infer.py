d4c7a0e524570819e5a3c729cfec05f4b8ff17bb
Yota Toyama <raviqqe@gmail.com>
2016-12-27 10:56:31 +0900

    Use def_def_infer_fn in infer module

--- qnd/infer.py
+++ qnd/infer.py
@@ -1,17 +1,17 @@
 from .estimator import def_estimator
 from .flag import FLAGS, add_flag, add_output_dir_flag
-from .inputs import def_def_eval_input_fn
+from .inputs import def_def_infer_input_fn
 
 
 def def_infer(batch_inputs=True, prepare_filename_queues=True):
     add_output_dir_flag()
 
     estimator = def_estimator()
-    def_eval_input_fn = def_def_eval_input_fn(batch_inputs,
-                                              prepare_filename_queues)
+    def_infer_input_fn = def_def_infer_input_fn(batch_inputs,
+                                                prepare_filename_queues)
 
     def infer(model_fn, input_fn):
         return estimator(model_fn, FLAGS.output_dir).predict(
-            input_fn=def_eval_input_fn(input_fn))
+            input_fn=def_infer_input_fn(input_fn))
 
     return infer
