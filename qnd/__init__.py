99c31e7a6c9f93f272f9831ae0c62bf073d7b334
Yota Toyama <raviqqe@gmail.com>
2017-01-17 17:00:15 +0900

    Bump version

--- qnd/__init__.py
+++ qnd/__init__.py
@@ -6,4 +6,4 @@ from .train_and_evaluate import def_train_and_evaluate
 
 __all__ = ["FLAGS", "add_flag", "add_required_flag", "FlagAdder",
            "def_train_and_evaluate", "def_infer"]
-__version__ = "0.0.12"
+__version__ = "0.0.13"
