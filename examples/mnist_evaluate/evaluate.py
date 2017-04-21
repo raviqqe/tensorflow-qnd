7c30378fca2b9fd6d9992279f7471485ce294639
Yota Toyama <raviqqe@gmail.com>
2017-04-22 00:05:42 +0900

    Catch tf.errors.OutOfRangeError on evaluation

--- examples/mnist_evaluate/evaluate.py
+++ examples/mnist_evaluate/evaluate.py
@@ -1,6 +1,7 @@
 import logging
 
 import qnd
+import tensorflow as tf
 
 import mnist
 
@@ -13,7 +14,11 @@ model = mnist.def_model()
 
 def main():
     logging.getLogger().setLevel(logging.INFO)
-    print(evaluate(model, mnist.read_file))
+
+    try:
+        print(evaluate(model, mnist.read_file))
+    except tf.errors.OutOfRangeError:
+        pass
 
 
 if __name__ == "__main__":
