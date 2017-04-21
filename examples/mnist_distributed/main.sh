d63040052949b5bbdf2abcfafcc5bebd0c07baaf
Yota Toyama <raviqqe@gmail.com>
2017-04-21 23:55:12 +0900

    Don't clean up automatically when something is wrong

--- examples/mnist_distributed/main.sh
+++ examples/mnist_distributed/main.sh
@@ -14,15 +14,8 @@ mnist() {
 }
 
 
-kill_servers() {
-  kill $(ps x | grep train.py | grep -v grep | awk '{print $1}')
-}
-
-
 main() {
   fetch_dataset || exit 1
-
-  kill_servers
 
   mnist ps > $var_dir/ps.log 2>&1 &
 
@@ -33,9 +26,7 @@ main() {
     worker_id=$(($worker_id + 1))
   done &&
 
-  mnist master &&
-
-  kill_servers
+  mnist master
 }
 
 
