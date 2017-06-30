b391737b7d885acd91af97749ea4be7412171ad8
Yota Toyama <raviqqe@gmail.com>
2017-06-30 20:46:09 +0900

    Bump version

--- qnd/__init__.py
+++ qnd/__init__.py
@@ -8,4 +8,4 @@ from .serve import def_serve
 
 __all__ = ["FLAGS", "add_flag", "add_required_flag", "FlagAdder",
            "def_train_and_evaluate", "def_evaluate", "def_infer", "def_serve"]
-__version__ = "0.1.7"
+__version__ = "0.1.8"
